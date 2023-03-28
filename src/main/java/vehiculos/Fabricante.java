6805bfb600b8fc3fced582bca4d507938e931452
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-03-28 14:15:45 -0500

    Create Fabricante.java

---
+++ src/main/java/vehiculos/Fabricante.java
@@ -0,0 +1,44 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+public class Fabricante {
+    protected String nombre;
+    protected Pais pais;
+    public int canVentas;
+    public static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+
+    public Fabricante(String nombre, Pais pais) {
+        this.pais = pais;
+        this.nombre = nombre;
+        fabricantes.add(this);
+    }
+
+    public static Fabricante fabricaMayorVentas() {
+        int iniciador = 0;
+        Fabricante fabricanteMayorVentas = null;
+        for (Fabricante fabricante : fabricantes) {
+            if (fabricante.canVentas > iniciador) {
+                iniciador = fabricante.canVentas;
+                fabricanteMayorVentas = fabricante;
+            }
+        }
+        return fabricanteMayorVentas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+}
