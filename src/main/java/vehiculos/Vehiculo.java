4f69c6f97105f6b28179c9bd2431ff29dc907324
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-03-28 14:25:05 -0500

    Update Vehiculo.java

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,69 +1,84 @@
 package vehiculos;
-
 public class Vehiculo {
+    protected String placa;
+    protected int puertas;
+    protected int velocidadMaxima;
     protected String nombre;
     protected int precio;
     protected int peso;
     protected String traccion;
     protected Fabricante fabricante;
-    protected static int CantidadVehiculos;
-    protected String placa;
-    protected int puertas;
-    protected int velocidadMaxima;
-  
-/* Clase Vehiculo */
-    public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,
-                    String traccion, Fabricante fabricante) {
+    private static int cantidadVehiculos;
+    
+    public Vehiculo(String placa,int puertas,int velocidadMaxima,String nombre,int precio,int peso,String traccion,Fabricante fabricante){
         this.placa = placa;
-        this.puertas = puertas;
-        this.velocidadMaxima = velocidadMaxima;
         this.nombre = nombre;
         this.precio = precio;
         this.peso = peso;
-        this.traccion = traccion;
         this.fabricante = fabricante;
-        CantidadVehiculos++;
-        if (Pais.ventasPorPais.containsKey(fabricante.getPais())) {
-            Pais.ventasPorPais.put(fabricante.getPais(), Pais.ventasPorPais.get(fabricante.getPais()) + 1);
-        } else {
-            Pais.ventasPorPais.put(fabricante.getPais(), 1);
-        }
-
-        if (Fabricante.ventasPorFabrica.containsKey(fabricante)) {
-            Fabricante.ventasPorFabrica.put(fabricante, Fabricante.ventasPorFabrica.get(fabricante) + 1);
-        } else {
-            Fabricante.ventasPorFabrica.put(fabricante, 1);
-        }
+        this.puertas = puertas;
+        this.velocidadMaxima = velocidadMaxima;
+        this.traccion = traccion;
+        fabricante.getPais().setCantidadVehiculos();
+        fabricante.setCantidadVehiculos();
+        ++cantidadVehiculos;
     }
-
-    public String getPlaca() {
+    public String getPlaca(){
         return placa;
     }
-    public int getPuertas() {
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+    public int getPuertas(){
         return puertas;
     }
-
+    public void setPuertas(int puertas){
+        this.puertas = puertas;
     }
-    public int getPeso() {
-        return peso;
-    }
-    public String getTraccion() {
-        return traccion;
-    }
-
-    public int getVelocidadMaxima() {
+    public int getVelocidadMaxima(){
         return velocidadMaxima;
     }
-    public String getNombre() {
+    public void setVelocidadMaxima(int velocidadMaxima){
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    public String getNombre(){
         return nombre;
     }
-    public int getPrecio() {
-        return precio;
-    public static int getCantidadVehiculos() {
-        return CantidadVehiculos;
+    public void setNombre(String nombre){
+        this.nombre = nombre;
     }
-
-    public static void setCantidadVehiculos(int cantidadVehiculos) {
-        CantidadVehiculos = cantidadVehiculos;
+    public int getPrecio(){
+        return precio;
+    }
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+    public int getPeso(){
+        return peso;
+    }
+    public void setPeso(int peso){
+        this.peso = peso;
+    }
+    public String getTraccion(){
+        return traccion;
+    }
+    public void setTraccion(String traccion){
+        this.traccion = traccion;
+    }
+    public Fabricante getFabricante(){
+        return fabricante;
+    }
+    public void setFabricante(Fabricante fabricante){
+        this.fabricante = fabricante;
+    }
+    public static int getCantidadVehiculos(){
+        return cantidadVehiculos;
+    }
+    public static void setCantidadVehiculos(int cantidadVehiculos){
+        Vehiculo.cantidadVehiculos = cantidadVehiculos;
+    }
+    public static String vehiculosPorTipo(){
+        String result = "Automoviles: " + Automovil.getCantidadAutomoviles() + "\nCamionetas: " + Camioneta.getcantidadCamionetas() + "\nCamiones: " + Camion.getCantidadCamiones();
+        return result;
     }
 }
