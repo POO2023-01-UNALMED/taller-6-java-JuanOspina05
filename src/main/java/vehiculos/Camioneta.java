cb1ea415d897937d25ccc1b2bc2da2c7113663af
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-03-28 14:11:35 -0500

    Create Camioneta.java

---
+++ src/main/java/vehiculos/Camioneta.java
@@ -0,0 +1,20 @@
+package vehiculos;
+
+public class Camioneta extends Vehiculo {
+    protected boolean volco;
+    public static int canCamionetas;
+
+    public Camioneta(String placa, int puertas, String nombre, double precio, int peso, Fabricante fabricante, boolean volco) {
+        super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
+        this.volco = volco;
+        canCamionetas++;
+    }
+
+    public boolean isVolco() {
+        return volco;
+    }
+
+    public void setVolco(boolean volco) {
+        this.volco = volco;
+    }
+}
