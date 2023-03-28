d041de0bf17ccc4891deea98e101d6d09fc14d4e
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-03-28 14:20:06 -0500

    Create Pais.java

---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,32 @@
+package vehiculos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pais {
+    private String nombre;
+    static Map<Pais, Integer> ventasPorPais = new HashMap<>();
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public static Pais paisMasVendedor() {
+        int valorMax = -1;
+        Pais paisMasVendedor = null;
+
+        for (Map.Entry<Pais, Integer> entry : ventasPorPais.entrySet()) {
+            final int valorActual = entry.getValue();
+
+            if (valorActual > valorMax) {
+                valorMax = valorActual;
+                paisMasVendedor = entry.getKey();
+            }
+        }
+        return paisMasVendedor;
+    }
+}
